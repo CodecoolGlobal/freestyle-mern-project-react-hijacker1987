06d7bc0766855fdeab8fffd50a9e1d767f66892d
Poszt Viktor <vposzt@gmail.com>
2023-06-21 09:11:27 +0200

    registration form going well

--- frontend/src/components/RegistrationForm.js
+++ frontend/src/components/RegistrationForm.js
@@ -18,31 +18,38 @@ export default function RegistrationForm() {
         expirationDate: '',
       });
 
-    const handleChange = (event) => {
-    const { name, value } = event.target;
+    const handleChange = (name, event) => {
+      const { value } = event.target;
+      setReg((prevReg) => ({ ...prevReg, [name]: value }));
+    }
+
+    const handleChangeDOB = (event) => {
+      const { name, value } = event.target;
     setReg((prevReg) => ({ ...prevReg, [name]: value }));
     };
+
+    console.log(reg);
 
   return (
     <div>
         Registration <br/>
-        name: <input placeholder="Your Name"></input> <br/>
-        date of birth: <input type="date" name="dateOfBirth" value={reg.dateOfBirth} onChange={handleChange}/> <br/>
+        name: <input onChange={(event) => handleChange("name", event)} placeholder="Your Name" />
+        date of birth: <input type="date" name="dateOfBirth" value={reg.dateOfBirth} onChange = { handleChangeDOB }/> <br/>
         gender: <select>
                     <option>Male</option>
                     <option>Female</option>
                 </select> <br/>
-        country: <input placeholder="In which country you live"/> <br/>
-        city: <input placeholder='In which city you live'/> <br/>
-        street: <input placeholder='In which street you are living'/> <br/>
-        username: <input placeholder='Your username to make login easier'/> <br/>
-        password: <input placeholder='Password'/> <br/>
-        confirm password: <input placeholder='Confirm password'/> <br/>
-        e-mail: <input placeholder='user@gmail.com'/> <br/>
-        tel: <input placeholder='+00-00-000-0000'/> <br/>
-        credit card number: <input placeholder='0000-0000-0000-0000'/> <br/>
-        cvc: <input placeholder='000'/> <br/>
-        expiration date: <input placeholder='01-26'/> <br/>
+        country: <input onChange={(event) => handleChange("country", event)} placeholder="In which country you live"/> <br/>
+        city: <input onChange={(event) => handleChange("ciry", event)} placeholder='In which city you live'/> <br/>
+        street: <input onChange={(event) => handleChange("street", event)} placeholder='In which street you are living'/> <br/>
+        username: <input onChange={(event) => handleChange("username", event)} placeholder='Your username to make login easier'/> <br/>
+        password: <input onChange={(event) => handleChange("password", event)} placeholder='Password'/> <br/>
+        confirm password: <input onChange={(event) => handleChange("confirmPassword", event)} placeholder='Confirm password'/> <br/>
+        e-mail: <input onChange={(event) => handleChange("email", event)} placeholder='user@gmail.com'/> <br/>
+        tel: <input onChange={(event) => handleChange("tel", event)} placeholder='+00-00-000-0000'/> <br/>
+        credit card number: <input onChange={(event) => handleChange("creditCardNumber", event)} placeholder='0000-0000-0000-0000'/> <br/>
+        cvc: <input onChange={(event) => handleChange("cvc", event)} placeholder='000'/> <br/>
+        expiration date: <input onChange={(event) => handleChange("expirationDate", event)} placeholder='01-26'/> <br/>
     </div>
   )
 }
